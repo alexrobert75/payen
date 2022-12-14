ef4403913551b7856e3167500bf0fca0bf8c2d46
alexrobert75 <117197013+alexrobert75@users.noreply.github.com>
2022-12-14 12:15:12 +0100

    Add files via upload

---
+++ core/slideshow.js
@@ -0,0 +1,23 @@
+// Slideshow contact
+
+const slide1 = document.getElementById("slide1");
+const slide2 = document.getElementById("slide2");
+
+const changeS = (elem1) => {
+    elem1.classList.toggle("slidepic2");
+}
+
+var isPaused = false;
+var time = 0;
+var t = setInterval(function() {
+    if(!isPaused) {
+        time++;
+        changeS(slide2);
+    }
+}, 3000);
+function play(){
+    isPaused = false;
+}
+function pause(){
+    isPaused = true;
+}
